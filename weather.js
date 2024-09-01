1046c37d7f9ed8790291f0d2436d43e79bcbe188
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2024-08-31 19:57:08 -0700

    Electron Implemented
    All the files added to make the Electron work as intended

---
+++ weather.js
@@ -0,0 +1,27 @@
+    // make screen darker during nighttime
+    // change background image or color depending on current weather or selected weather
+    // offer fahrenheit and celsius 
+    // cookies for reacent weahter options and recent searches
+    // if you click enter, search weather
+    //throw error if name of city bad
+    //git ignore should hide key
+    function readCity(){
+        e.preventDefault();
+        console.log("HELLO")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    //ADDING SCRIPTS TO THE HTML
+    document.getElementById('submitButton').addEventListener("click",function(){
+        console.log("Hello")
+    })
