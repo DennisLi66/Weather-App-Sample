cc9de121c955337d6fe25e5f7ff1c27192dd047e
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2024-09-01 18:47:20 -0700

    Moved fetch to renderer instead of at preload
    changed todolist
    fetch works right now but only presents the first day in an ugly format

--- preload.js
+++ preload.js
@@ -13,23 +13,5 @@ try {
   }
 
 contextBridge.exposeInMainWorld('fL', {
-  contactWeatherAPI: (location,startDate,endDate)=>{
-    const date = new Date();
-    //need yyyy-mm-dd
-    var today = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
-    date.setDate(date.getDate()+4);
-    var later = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
-    var apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${today}/${later}?key=${API_KEY}`;
-    // Example    https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2020-10-01/2020-12-31?key=YOUR_API_KEY
-    // console.log(apiURL); 
-    fetch(apiURL)
-      .then(response => {
-        console.log(response.json());
-        return response.json();
-      })
-      .catch(error => {
-        console.log(error);
-        return false;
-      })
-  }
+  API_KEY: API_KEY
 })
--- renderer.js
+++ renderer.js
@@ -1,35 +1,55 @@
-// import { readFileSync } from 'fs';
-// import { load } from 'js-yaml'; //https://www.npmjs.com/package/js-yaml
-// make screen darker during nighttime
-// change background image or color depending on current weather or selected weather
-// offer fahrenheit and celsius 
-// cookies for reacent weahter options and recent searches
+// make screen darker during nighttime //Less Feasible
+// change background image or color depending on weather of current day
+// offer fahrenheit and celsius  //Unneccessary but doable
+// cookies for reacent weahter options and recent searches //unlikely
 // if you click enter, search weather
-//throw error if name of city bad
-//git ignore should hide key
 //give an array if times and days
 function changeStyleForWeather(weather,temperature,celOrFahr,timeOfDay){
     //change the style of the screen based on above params
 }
-function readCity(){
+async function readCity(){
     //get city
     var city = document.getElementById('cityName').value;
     if (!city) {
-        alert("Please enter a city name.");
+        alert("Please enter a valid city name.");
         window.location.reload();
     }
     //contact the api
-    var temperature = 50; //FIX THIS
-    var weather = "sunny";
-    var celOrFahr = "Celsius"
-    var timeOfDay = "10:00PM";
-    document.getElementById('content').innerHTML =
-    `
-    <h1>${city}</h1>
-    <h4>It is currently ${timeOfDay} in ${city}.</h4>
-    <h4>The weather right now is ${weather}. </h4>
-    <h4>The temperature is ${temperature}° ${celOrFahr}. </h4>
-    `;
+    const date = new Date();
+    var today = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`; //need yyyy-mm-dd
+    date.setDate(date.getDate()+4);
+    var later = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+    var apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${today}/${later}?key=${fL.API_KEY}`;
+    // Example    https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2020-10-01/2020-12-31?key=YOUR_API_KEY
+    // console.log(apiURL); 
+    await fetch(apiURL)
+      .then(response => {
+        if (response.status === 400){
+            alert("Please enter a valid city name.");
+            window.location.reload(); 
+        }else{
+            response.json().then(json => {
+                console.log(json.days);
+                var temperature = json.days[0].temp;
+                var weather = json.days[0].conditions;
+                var celOrFahr = "Fahrenheit" //FIX THIS
+                var timeOfDay = "10:00PM"; //FIX THIS
+                document.getElementById('content').innerHTML =
+                `
+                <h1>${city}</h1>
+                <h4>It is currently ${timeOfDay} in ${city}.</h4>
+                <h4>The weather right now is ${weather}. </h4>
+                <h4>The temperature is ${temperature}° ${celOrFahr}. </h4>
+                `;
+            }).catch(error => {
+                console.log(error);
+                return false;     
+            });
+        }
+      }).catch(error => {
+        console.log(error);
+        return false;
+      })
 }
 
 //ADDING SCRIPTS TO THE HTML
