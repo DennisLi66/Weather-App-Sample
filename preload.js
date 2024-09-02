14992d89303b5e3f70c8fee1ecae82bc496926af
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2024-09-01 18:07:29 -0700

    Added ability to fetch with the weather API URL
    Updated Security Policy
    Set it 5 day range properly

--- preload.js
+++ preload.js
@@ -17,18 +17,19 @@ contextBridge.exposeInMainWorld('fL', {
     const date = new Date();
     //need yyyy-mm-dd
     var today = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
-    date.setDate(date.getDate()+5);
+    date.setDate(date.getDate()+4);
     var later = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
     var apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${today}/${later}?key=${API_KEY}`;
     // Example    https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2020-10-01/2020-12-31?key=YOUR_API_KEY
     // console.log(apiURL); 
-    // fetch(apiURL)
-    //   .then(response => {
-
-    //   })
-    //   .catch(error => {
-    //     console.log(error);
-    //     return false;
-    //   })
+    fetch(apiURL)
+      .then(response => {
+        console.log(response.json());
+        return response.json();
+      })
+      .catch(error => {
+        console.log(error);
+        return false;
+      })
   }
 })
